7391f90333d7795a47c478148ae247443967e453
Augusto Inacio <augustoinacio@gmail.com>
2017-10-11 20:31:27 -0300

    criacao conexao com mysql

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,5 +1,13 @@
+var dbConnection = require ('../infra/connectionFactory');
 module.exports = function (app) {
     app.get('/produtos', function (req, res) {
-        res.render('produtos/lista');
+        var connection = dbConnection();
+        connection.query('Select * from produtos',function(err,result){
+            console.log(result);
+            console.log('------------');
+            res.render('produtos/lista',{lista:result});
+        });
+
+        connection.end();
     });
 }
